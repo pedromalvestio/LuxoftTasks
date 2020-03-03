f0138b9428d2b95444fa4c321547493c1c2a677c
Pedro Note Home <pedro.malvestio@gmail.com>
2020-03-03 01:10:01 -0300

    Adicionar arquivos de projeto.

---
+++ LuxoftTasks/FirstTask/MaxSum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuxoftTasks.FirstTask
+{
+    class MaxSum
+    {
+        public int FindSum(int[] arr)
+        {
+            //Your solution goes here
+            return 0;
+        }
+        static int FindMaxSum(int[] arr)
+        {
+            int incl = arr[0];
+            int excl = 0;
+            int excl_new;
+            int i;
+
+            for (i = 1; i < arr.Length; i++)
+            {
+                /* current max excluding i */
+                excl_new = (incl > excl) ? incl : excl;
+
+                /* current max including i */
+                incl = excl + arr[i];
+                excl = excl_new;
+            }
+
+            /* return max of incl and excl */
+            return ((incl > excl) ? incl : excl);
+        }
+    }
+}
+
---
+++ LuxoftTasks/SecondTask/DeadPixels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuxoftTasks.SecondTask
+{
+    class DeadPixels
+    {
+        public int CountGroups(char[][] monitor)
+        {
+            //Your solution goes here
+
+            //https://www.hackerrank.com/challenges/connected-cell-in-a-grid/problem
+
+            //https://gist.github.com/jianminchen/4f4d499599c015144e77e5016bd86912
+
+            return 0;
+        }
+    }
+}
