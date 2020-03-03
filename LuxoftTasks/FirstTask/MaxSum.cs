141054e7d461f38bca643be61baeb5588036c0f0
Pedro Note Home <pedro.malvestio@gmail.com>
2020-03-03 01:21:57 -0300

    Initial Functions Creation

--- LuxoftTasks/FirstTask/MaxSum.cs
+++ LuxoftTasks/FirstTask/MaxSum.cs
@@ -1,24 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LuxoftTasks.FirstTask
+﻿namespace LuxoftTasks.FirstTask
 {
     class MaxSum
     {
         public int FindSum(int[] arr)
         {
-            //Your solution goes here
-            return 0;
-        }
-        static int FindMaxSum(int[] arr)
-        {
             int incl = arr[0];
             int excl = 0;
             int excl_new;
-            int i;
 
-            for (i = 1; i < arr.Length; i++)
+            for (int i = 1; i < arr.Length; i++)
             {
                 /* current max excluding i */
                 excl_new = (incl > excl) ? incl : excl;
--- LuxoftTasks/SecondTask/DeadPixels.cs
+++ LuxoftTasks/SecondTask/DeadPixels.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace LuxoftTasks.SecondTask
 {
@@ -15,6 +13,76 @@ namespace LuxoftTasks.SecondTask
             //https://gist.github.com/jianminchen/4f4d499599c015144e77e5016bd86912
 
             return 0;
+        }
+
+        public static int getMax(int[][] arr, int m, int n)
+        {
+            bool[][] visited = new bool[m][];
+
+            for (int i = 0; i < m; i++)
+                visited[i] = new bool[n];
+
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    visited[i][j] = false;
+                }
+            int max = 0;
+            IList<string> curList = new List<string>();
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    if (!visited[i][j])
+                    {
+                        int node = arr[i][j];
+                        if (node == 1)
+                        {
+                            IList<string> list = new List<string>();
+
+                            DFS(arr, i, j, visited, m + n, list);
+
+                            if (list.Count > max)
+                            {
+                                curList = list;
+                                max = list.Count;
+                            }
+                        }
+                    }
+
+                }
+            return max;
+        }
+
+        private static void DFS(int[][] arr, int i, int j, bool[][] visited, int maxStep, IList<string> list)
+        {
+            int row = arr.Length;
+            int col = arr[0].Length;
+            if (i < row && i >= 0 && j < col && j >= 0 && maxStep >= 0)
+            {
+                // node is 1 and not visited. 
+                if (arr[i][j] == 1 && !visited[i][j])
+                {
+                    visited[i][j] = true;
+
+                    list.Add(i.ToString() + ";" + j.ToString());
+
+                    DFS(arr, i - 1, j - 1, visited, maxStep - 1, list);
+                    DFS(arr, i - 1, j, visited, maxStep - 1, list);
+                    DFS(arr, i - 1, j + 1, visited, maxStep - 1, list);
+
+                    DFS(arr, i, j - 1, visited, maxStep - 1, list);
+                    DFS(arr, i, j + 1, visited, maxStep - 1, list);
+
+                    DFS(arr, i + 1, j - 1, visited, maxStep - 1, list);
+                    DFS(arr, i + 1, j, visited, maxStep - 1, list);
+                    DFS(arr, i + 1, j + 1, visited, maxStep - 1, list);
+                }
+                else if (arr[i][j] == 0)
+                {
+                    if (!visited[i][j])
+                        visited[i][j] = true;
+                }
+            }
         }
     }
 }
